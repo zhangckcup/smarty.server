a51a01a86327e78abd24b52555fdb937c2793efe
zhangckcup <zhangckcup@qq.com>
2020-04-06 09:07:33 +0800

    sql init

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const mysqlDB = require("./module")
 const port = 3000;
 
+mysqlDB.query("SELECT * from users where user", (err, row, fields) => {
+  if(err){
+    console.error(err)
+  }
 
+})
 
 app.listen(port, () => console.log(`Server run in port ${port}`))
