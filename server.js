937fa32d746d873d5211a05d0dc514554ab6efb6
zhangckcup <zhangckcup@qq.com>
2020-04-06 09:52:10 +0800

    利用 Express 托管静态文件

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const app = express();
-const mysqlDB = require("./module")
+const mysqlDB = require("./module/module")
 const port = 3000;
 
-mysqlDB.query("SELECT * from users where user", (err, row, fields) => {
-  if(err){
-    console.error(err)
-  }
-
-})
+app.use(express.static('dist'))
 
 app.listen(port, () => console.log(`Server run in port ${port}`))
